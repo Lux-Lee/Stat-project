6022c31941f7b1071d6bd44fb764d0761f17485b
Lux-Lee <95jooyeon@gmail.com>
2024-03-28 22:24:48 -0400

    MAr28

--- Code4.R
+++ Code4.R
@@ -1,9 +1,13 @@
+library(readxl)
+Canadian_Health <- read_excel("Proj_data.xlsx", sheet = "Canadian Perceived Health")
+Indigenous_Health <- read_excel("Proj_data.xlsx", sheet = "Indigenous Perceived Health")
+
 Health_Data  <- bind_rows(
   mutate(Indigenous_Health, Group = "Indigenous"),
   mutate(Canadian_Health, Group = "Canadian")
 )
 
-Perceived <- Health_Data %>%                          ## Data formatting and factoring
+Perceived <- Health_Data %>%                         
   filter(Gender != "Both") %>%
   mutate(
     Percentage = Percentage / 100,
@@ -12,3 +16,12 @@ Perceived <- Health_Data %>%                          ## Data formatting and fac
     Group = factor(Group, levels = c("Canadian", "Indigenous")),
     Category = factor(Category, levels = c("Fair+Poor", "Good", "Excellent+Very good"), ordered = TRUE)
   )
+
+longData <- Perceived %>%                      
+  group_by(Year, Gender, Category, Group) %>%
+  summarize(1 ,response = rep(1, Count)) %>%
+  ungroup()
+
+model1 <- multinom(Category ~ Gender * Group, data = longData)
+summary(model1)
+modelReduced <- multinom(Category ~ Gender + Group, data = longData)
