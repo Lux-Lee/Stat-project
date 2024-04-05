a5e6568d1130b95a98ececa0f612ca57472781d1
Lux-Lee <95jooyeon@gmail.com>
2024-04-05 11:23:21 -0400

    Apr5

--- Final Code.R
+++ Final Code.R
@@ -28,15 +28,18 @@ summary(glm1)
 ####Estimated marginal means####
 ##Health care disparty change in groups##
 emm2 <- emmeans(glm1, ~ Group, by = "Period", type = "response")
-plot(emm2, xlab="Ex_G Proportion", ylab="Category")
+plot(emm2, xlab="Ex_G Proportion", ylab="Category", color="yellow") +
+  theme_black() 
 summary(emm2)
 ##Change in genders##
 emm4 <- emmeans(glm1, ~ Gender, by = "Period", type = "response")
-plot(emm4, xlab="Ex_G Proportion", ylab="Category")
+plot(emm4, xlab="Ex_G Proportion", ylab="Category", color="yellow") +
+  theme_black() 
 summary(emm4)
 ##Gender difference change among groups##
 emm3<- emmeans(glm1, ~ Gender + Period, by="Group", type="response")
-plot(emm3, xlab="Ex_G Proportion", ylab="Category")
+plot(emm3, xlab="Ex_G Proportion", ylab="Category", color="yellow") +
+  theme_black() 
 summary(emm3)
 emm3_D<- data.frame(emm3)
 ##Overall change##
@@ -44,7 +47,8 @@ emm1<- emmeans(glm1, ~Gender + Period + Group, type = "response")
 pairs(emm1)
 summary(emm1)
 emm1_D<- data.frame(emm1)
-plot(emm1, xlab="Ex_G Proportion", ylab="Category")
+plot(emm1, xlab="Ex_G Proportion", ylab="Category", color="yellow") +
+  theme_black() 
 ##Plotting##
 emm1_D<-data.frame(emm1_D[1:4])
 emm1_D <-emm1_D %>%
@@ -54,9 +58,60 @@ emm1_D <-emm1_D %>%
 )
 emm1_D <-emm1_D %>%
   group_by(Gender, Group)
-ggplot(data=emm1_D, aes(color=Gender,lty=Group, x=Period, y=prob)) +
+ggplot(data=emm1_D, aes(color=interaction(Gender, Group), x=Period, y=prob)) +
+  theme_black() + 
   labs(title="COVID-19 impact on Perceived Health",
-       x="Period", y="Probability of Excellent_Good") +
+       x="Period", y="Probability of Excellent_Good", color="Gender.Group") +
   geom_point() +
   geom_line(data=subset(emm1_D, Group=="Canadian"),aes(group=Gender)) +
-  geom_line(data=subset(emm1_D, Group=="Indigenous"),aes(group=Gender))
+  geom_line(data=subset(emm1_D, Group=="Indigenous"),aes(group=Gender)) 
+
+
+library(gridExtra)
+
+theme_black <- function(base_size = 12, base_family = "") {
+  
+  theme_grey(base_size = base_size, base_family = base_family) %+replace%
+    
+    theme(
+      line= element_line(color="white"),
+      # Specify axis options
+      axis.line = element_blank(),  
+      axis.text.x = element_text(size = base_size*0.8, color = "white", lineheight = 0.9),  
+      axis.text.y = element_text(size = base_size*0.8, color = "white", lineheight = 0.9),  
+      axis.ticks = element_line(color = "white", size  =  0.2),  
+      axis.title.x = element_text(size = base_size, color = "white", margin = margin(0, 10, 0, 0)),  
+      axis.title.y = element_text(size = base_size, color = "white", angle = 90, margin = margin(0, 10, 0, 0)),  
+      axis.ticks.length = unit(0.3, "lines"),   
+      # Specify legend options
+      legend.background = element_rect(color = NA, fill = "black"),  
+      legend.key = element_rect(color = "white",  fill = "black"),  
+      legend.key.size = unit(1.2, "lines"),  
+      legend.key.height = NULL,  
+      legend.key.width = NULL,      
+      legend.text = element_text(size = base_size*0.8, color = "white"), 
+      legend.axis.line = element_line(color="white"),
+      legend.title = element_text(size = base_size*0.8, face = "bold", hjust = 0, color = "white"),  
+      legend.position = "right",  
+      legend.text.align = NULL,  
+      legend.title.align = NULL,  
+      legend.direction = "vertical",  
+      legend.box = NULL, 
+      # Specify panel options
+      panel.background = element_rect(fill = "black", color  =  NA),  
+      panel.border = element_rect(fill = NA, color = "white"),  
+      panel.grid.major = element_line(color = "grey35"),  
+      panel.grid.minor = element_line(color = "grey20"),  
+      panel.margin = unit(0.5, "lines"),   
+      # Specify facetting options
+      strip.background = element_rect(fill = "grey30", color = "grey10"),  
+      strip.text.x = element_text(size = base_size*0.8, color = "white"),  
+      strip.text.y = element_text(size = base_size*0.8, color = "white",angle = -90),  
+      # Specify plot options
+      plot.background = element_rect(color = "black", fill = "black"),  
+      plot.title = element_text(size = base_size*1.2, color = "white"),  
+      plot.margin = unit(rep(1, 4), "lines")
+      
+    )
+  
+}
